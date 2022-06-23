90d4cd04725ee206d9bc1c4bcabe1df0384751b4
Renan Perin Ribeiro <renan.perinribeiro@outlook.com>
2022-06-23 19:27:32 -0300

    update

---
+++ costs/src/components/pages/Company.js
@@ -0,0 +1,7 @@
+function Company() {
+    return (
+        <h1>Home</h1>
+    )
+}
+
+export default Company
---
+++ costs/src/components/pages/Contact.js
@@ -0,0 +1,7 @@
+function Contact() {
+    return (
+        <h1>Home</h1>
+    )
+}
+
+export default Contact
