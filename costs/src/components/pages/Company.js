8a07ff4d476cc464d99e289885f23e225786f727
Renan Perin Ribeiro <renan.perinribeiro@outlook.com>
2022-06-30 18:38:41 -0300

    update

--- costs/src/components/pages/Company.js
+++ costs/src/components/pages/Company.js
@@ -1,6 +1,13 @@
+import styles from './Company.module.css'
+import renan from "../../img/renanperin.jpeg"
+
 function Company() {
     return (
-        <h1>Home</h1>
+        <div className={styles.home_container}>
+            <h1>Renan Perin <span>Company</span></h1>
+            <p>Estudante de React.js, querendo aprender tudo sobre este framework!</p>
+            <img src={renan} alt = "Renan"/>
+        </div>
     )
 }
 
--- costs/src/components/pages/Contact.js
+++ costs/src/components/pages/Contact.js
@@ -1,6 +1,20 @@
+import styles from './Contact.module.css'
+import {FaFacebook, FaInstagram, FaLinkedin} from "react-icons/fa"
+
 function Contact() {
     return (
-        <h1>Home</h1>
+        <div className={styles.home_container}>
+            <h1>Entre em <span>contato</span> comigo</h1>
+            <a href="https://pt-br.facebook.com/" target="_blank" rel="noreferrer">
+                <h3>Face<span>book</span></h3>
+            </a>
+            <a href='https://www.instagram.com/perin_renan/' target="_blank" rel="noreferrer">
+                <h3>Insta<span>gram</span></h3>
+            </a>
+            <a href="https://www.linkedin.com/in/renan-perin-ribeiro-483443231/" target="_blank" rel="noreferrer">
+                <h3>Linked<span>in</span></h3> 
+            </a>            
+        </div>
     )
 }
 
